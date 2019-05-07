7d897b493a688a3e5980b22f9b849981ad8df3d9
bushifeng <fengmengkun@icloud.com>
2019-05-07 15:41:55 +0800

    ceshi1

---
+++ ly-auth/ly-auth-service/src/test/java/com/test/Test1.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bushifeng
+ * @version v1.0
+ * @date 2019/4/22 17:26
+ * @description TODO
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        String s="abc";
+        s.replace("a", "b");
+        System.out.println(s);
+    }
+}
